crspeller/mattermost-server

Modifying permissions system.

api/authorization_test.go
@@ -1,0 +1,40 @@
+// Copyright (c) 2016 Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+package api
+
+import (
+	"testing"
+
+	"github.com/mattermost/platform/model"
+)
+
+/*func TestCheckIfRolesGrantPermission(t *testing.T) {
+	th := Setup().InitBasic()
+}*/
+
+func TestCheckIfRolesGrantPermission(t *testing.T) {
+	Setup()
+
+	cases := []struct {
+		roles        []string
+		permissionId string
+		shouldGrant  bool
+	}{
+		{[]string{model.R_SYSTEM_ADMIN.Id}, model.R_SYSTEM_ADMIN.Permissions[0], true},
+		{[]string{model.R_SYSTEM_ADMIN.Id}, "non-existant-permission", false},
+		{[]string{model.R_CHANNEL_USER.Id}, model.R_CHANNEL_USER.Permissions[0], true},
+		{[]string{model.R_CHANNEL_USER.Id}, model.P_MANAGE_SYSTEM.Id, false},
+		{[]string{model.R_SYSTEM_ADMIN.Id, model.R_CHANNEL_USER.Id}, model.P_MANAGE_SYSTEM.Id, true},
+		{[]string{model.R_CHANNEL_USER.Id, model.R_SYSTEM_ADMIN.Id}, model.P_MANAGE_SYSTEM.Id, true},
+		{[]string{model.R_TEAM_USER.Id, model.R_TEAM_ADMIN.Id}, model.P_MANAGE_SLASH_COMMANDS.Id, true},
+		{[]string{model.R_TEAM_ADMIN.Id, model.R_TEAM_USER.Id}, model.P_MANAGE_SLASH_COMMANDS.Id, true},
+	}
+
+	for testnum, testcase := range cases {
+		if CheckIfRolesGrantPermission(testcase.roles, testcase.permissionId) != testcase.shouldGrant {
+			t.Fatal("Failed test case ", testnum)
+		}
+	}
+
+}

api/context.go
@@ -16,7 +16,6 @@ import (
 
 	"github.com/mattermost/platform/einterfaces"
 	"github.com/mattermost/platform/model"
-	"github.com/mattermost/platform/store"
 	"github.com/mattermost/platform/utils"
 )
 
@@ -75,6 +74,10 @@ func AppHandlerTrustRequester(h func(*Context, http.ResponseWriter, *http.Reques
 
 func ApiAdminSystemRequired(h func(*Context, http.ResponseWriter, *http.Request)) http.Handler {
 	return &handler{h, true, true, true, false, false, false}
+}
+
+func ApiAdminSystemRequiredTrustRequester(h func(*Context, http.ResponseWriter, *http.Request)) http.Handler {
+	return &handler{h, true, true, true, false, false, true}
 }
 
 func ApiAppHandlerTrustRequester(h func(*Context, http.ResponseWriter, *http.Request)) http.Handler {
@@ -202,10 +205,6 @@ func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		c.SystemAdminRequired()
 	}
 
-	if c.Err == nil && len(c.TeamId) > 0 && !h.isTeamIndependent {
-		c.HasPermissionsToTeam(c.TeamId, "TeamRoute")
-	}
-
 	if c.Err == nil && h.isUserActivity && token != "" && len(c.Session.UserId) > 0 {
 		SetStatusOnline(c.Session.UserId, c.Session.Id)
 	}
@@ -320,88 +319,11 @@ func (c *Context) SystemAdminRequired() {
 		c.Err = model.NewLocAppError("", "api.context.session_expired.app_error", nil, "SystemAdminRequired")
 		c.Err.StatusCode = http.StatusUnauthorized
 		return
-	} else if !c.IsSystemAdmin() {
+	} else if !HasPermissionToContext(c, model.P_MANAGE_SYSTEM) {
 		c.Err = model.NewLocAppError("", "api.context.permissions.app_error", nil, "AdminRequired")
 		c.Err.StatusCode = http.StatusForbidden
 		return
 	}
-}
-
-func (c *Context) HasPermissionsToUser(userId string, where string) bool {
-
-	// You are the user
-	if c.Session.UserId == userId {
-		return true
-	}
-
-	// You're a mattermost system admin and you're on the VPN
-	if c.IsSystemAdmin() {
-		return true
-	}
-
-	c.Err = model.NewLocAppError(where, "api.context.permissions.app_error", nil, "userId="+userId)
-	c.Err.StatusCode = http.StatusForbidden
-	return false
-}
-
-func (c *Context) HasPermissionsToTeam(teamId string, where string) bool {
-	if c.IsSystemAdmin() {
-		return true
-	}
-
-	for _, teamMember := range c.Session.TeamMembers {
-		if teamId == teamMember.TeamId {
-			return true
-		}
-	}
-
-	c.Err = model.NewLocAppError(where, "api.context.permissions.app_error", nil, "userId="+c.Session.UserId+", teamId="+teamId)
-	c.Err.StatusCode = http.StatusForbidden
-	return false
-}
-
-func (c *Context) HasPermissionsToChannel(sc store.StoreChannel, where string) bool {
-	if cresult := <-sc; cresult.Err != nil {
-		c.Err = cresult.Err
-		return false
-	} else if cresult.Data.(int64) != 1 {
-		c.Err = model.NewLocAppError(where, "api.context.permissions.app_error", nil, "userId="+c.Session.UserId)
-		c.Err.StatusCode = http.StatusForbidden
-		return false
-	}
-
-	return true
-}
-
-func (c *Context) HasSystemAdminPermissions(where string) bool {
-	if c.IsSystemAdmin() {
-		return true
-	}
-
-	c.Err = model.NewLocAppError(where, "api.context.system_permissions.app_error", nil, "userId="+c.Session.UserId)
-	c.Err.StatusCode = http.StatusForbidden
-	return false
-}
-
-func (c *Context) IsSystemAdmin() bool {
-	if model.IsInRole(c.Session.Roles, model.ROLE_SYSTEM_ADMIN) {
-		return true
-	}
-	return false
-}
-
-func (c *Context) IsTeamAdmin() bool {
-
-	if c.IsSystemAdmin() {
-		return true
-	}
-
-	teamMember := c.Session.GetTeamByTeamId(c.TeamId)
-	if teamMember == nil {
-		return false
-	}
-
-	return teamMember.IsTeamAdmin()
 }
 
 func (c *Context) RemoveSessionCookie(w http.ResponseWriter, r *http.Request) {
@@ -461,6 +383,10 @@ func (c *Context) GetTeamURL() string {
 
 func (c *Context) GetSiteURL() string {
 	return c.siteURL
+}
+
+func (c *Context) GetCurrentTeamMember() *model.TeamMember {
+	return c.Session.GetTeamByTeamId(c.TeamId)
 }
 
 func IsApiCall(r *http.Request) bool {

api/emoji.go
@@ -182,7 +182,7 @@ func deleteEmoji(c *Context, w http.ResponseWriter, r *http.Request) {
 		c.Err = result.Err
 		return
 	} else {
-		if c.Session.UserId != result.Data.(*model.Emoji).CreatorId && !c.IsSystemAdmin() {
+		if c.Session.UserId != result.Data.(*model.Emoji).CreatorId && !HasPermissionToContext(c, model.P_MANAGE_SYSTEM) {
 			c.Err = model.NewLocAppError("deleteEmoji", "api.emoji.delete.permissions.app_error", nil, "user_id="+c.Session.UserId)
 			c.Err.StatusCode = http.StatusUnauthorized
 			return

api/post.go
@@ -58,10 +58,24 @@ func createPost(c *Context, w http.ResponseWriter, r *http.Request) {
 	}
 	post.UserId = c.Session.UserId
 
-	// Create and save post object to channel
-	cchan := Srv.Store.Channel().CheckPermissionsTo(c.TeamId, post.ChannelId, c.Session.UserId)
+	cchan := Srv.Store.Channel().Get(post.ChannelId)
 
-	if !c.HasPermissionsToChannel(cchan, "createPost") {
+	if !HasPermissionToChannelContext(c, post.ChannelId, model.P_CREATE_POST) {
+		return
+	}
+
+	// Check that channel has not been deleted
+	var channel *model.Channel
+	if result := <-cchan; result.Err != nil {
+		c.SetInvalidParam("createPost", "post.channelId")
+		return
+	} else {
+		channel = result.Data.(*model.Channel)
+	}
+
+	if channel.DeleteAt != 0 {
+		c.Err = model.NewLocAppError("createPost", "api.post.create_post.can_not_post_to_deleted.error", nil, "")
+		c.Err.StatusCode = http.StatusBadRequest
 		return
 	}
 
@@ -1003,10 +1017,9 @@ func updatePost(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	cchan := Srv.Store.Channel().CheckPermissionsTo(c.TeamId, post.ChannelId, c.Session.UserId)
 	pchan := Srv.Store.Post().Get(post.Id)
 
-	if !c.HasPermissionsToChannel(cchan, "updatePost") {
+	if !HasPermissionToChannelContext(c, post.ChannelId, model.P_EDIT_POST) {
 		return
 	}
 
@@ -1108,10 +1121,9 @@ func getPosts(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	cchan := Srv.Store.Channel().CheckPermissionsTo(c.TeamId, id, c.Session.UserId)
 	etagChan := Srv.Store.Post().GetEtag(id)
 
-	if !c.HasPermissionsToChannel(cchan, "getPosts") {
+	if !HasPermissionToChannelContext(c, id, model.P_CREATE_POST) {
 		return
 	}
 
@@ -1150,10 +1162,9 @@ func getPostsSince(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	cchan := Srv.Store.Channel().CheckPermissionsTo(c.TeamId, id, c.Session.UserId)
 	pchan := Srv.Store.Post().GetPostsSince(id, time)
 
-	if !c.HasPermissionsToChannel(cchan, "getPostsSince") {
+	if !HasPermissionToChannelContext(c, id, model.P_READ_CHANNEL) {
 		return
 	}
 
@@ -1183,10 +1194,9 @@ func getPost(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	cchan := Srv.Store.Channel().CheckPermissionsTo(c.TeamId, channelId, c.Session.UserId)
 	pchan := Srv.Store.Post().Get(postId)
 
-	if !c.HasPermissionsToChannel(cchan, "getPost") {
+	if !HasPermissionToChannelContext(c, channelId, model.P_READ_CHANNEL) {
 		return
 	}
 
@@ -1230,8 +1240,7 @@ func getPostById(c *Context, w http.ResponseWriter, r *http.Request) {
 		}
 		post := list.Posts[list.Order[0]]
 
-		cchan := Srv.Store.Channel().CheckPermissionsTo(c.TeamId, post.ChannelId, c.Session.UserId)
-		if !c.HasPermissionsToChannel(cchan, "getPostById") {
+		if !HasPermissionToChannelContext(c, post.ChannelId, model.P_READ_CHANNEL) {
 			return
 		}
 
@@ -1265,12 +1274,7 @@ func getPermalinkTmp(c *Context, w http.ResponseWriter, r *http.Request) {
 		}
 		post := list.Posts[list.Order[0]]
 
-		if !c.HasPermissionsToTeam(c.TeamId, "permalink") {
-			return
-		}
-
-		cchan := Srv.Store.Channel().CheckPermissionsTo(c.TeamId, post.ChannelId, c.Session.UserId)
-		if !c.HasPermissionsToChannel(cchan, "getPermalinkTmp") {
+		if !HasPermissionToChannelContext(c, post.ChannelId, model.P_READ_CHANNEL) {
 			// If we don't have permissions attempt to join the channel to fix the problem
 			if err, _ := JoinChannelById(c, c.Session.UserId, post.ChannelId); err != nil {
 				// On error just return with permissions error
@@ -1306,7 +1310,10 @@ func deletePost(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	cchan := Srv.Store.Channel().CheckPermissionsTo(c.TeamId, channelId, c.Session.UserId)
+	if !HasPermissionToChannelContext(c, channelId, model.P_EDIT_POST) {
+		return
+	}
+
 	pchan := Srv.Store.Post().Get(postId)
 
 	if result := <-pchan; result.Err != nil {
@@ -1315,10 +1322,6 @@ func deletePost(c *Context, w http.ResponseWriter, r *http.Request) {
 	} else {
 
 		post := result.Data.(*model.PostList).Posts[postId]
-
-		if !c.HasPermissionsToChannel(cchan, "deletePost") && !c.IsTeamAdmin() {
-			return
-		}
 
 		if post == nil {
 			c.SetInvalidParam("deletePost", "postId")
@@ -1331,7 +1334,7 @@ func deletePost(c *Context, w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		if post.UserId != c.Session.UserId && !c.IsTeamAdmin() {
+		if post.UserId != c.Session.UserId && !HasPermissionToChannelContext(c, post.ChannelId, model.P_EDIT_OTHERS_POSTS) {
 			c.Err = model.NewLocAppError("deletePost", "api.post.delete_post.permissions.app_error", nil, "")
 			c.Err.StatusCode = http.StatusForbidden
 			return
@@ -1411,11 +1414,10 @@ func getPostsBeforeOrAfter(c *Context, w http.ResponseWriter, r *http.Request, b
 		return
 	}
 
-	cchan := Srv.Store.Channel().CheckPermissionsTo(c.TeamId, id, c.Session.UserId)
 	// We can do better than this etag in this situation
 	etagChan := Srv.Store.Post().GetEtag(id)
 
-	if !c.HasPermissionsToChannel(cchan, "getPostsBeforeOrAfter") {
+	if !HasPermissionToChannelContext(c, id, model.P_READ_CHANNEL) {
 		return
 	}
 

api/team_test.go
@@ -175,14 +175,14 @@ func TestRemoveUserFromTeam(t *testing.T) {
 		t.Fatal("should fail not enough permissions")
 	} else {
 		if err.Id != "api.context.permissions.app_error" {
-			t.Fatal("wrong error")
+			t.Fatal("wrong error. Got: " + err.Id)
 		}
 	}
 
 	if _, err := th.BasicClient.RemoveUserFromTeam("", th.SystemAdminUser.Id); err == nil {
 		t.Fatal("should fail not enough permissions")
 	} else {
-		if err.Id != "api.team.update_team.permissions.app_error" {
+		if err.Id != "api.context.permissions.app_error" {
 			t.Fatal("wrong error")
 		}
 	}
@@ -318,7 +318,7 @@ func TestGetAllTeamListings(t *testing.T) {
 	c := &Context{}
 	c.RequestId = model.NewId()
 	c.IpAddress = "cmd_line"
-	UpdateUserRoles(c, user, model.ROLE_SYSTEM_ADMIN)
+	UpdateUserRoles(c, user, model.R_SYSTEM_ADMIN.Id)
 
 	Client.Login(user.Email, "passwd1")
 	Client.SetTeamId(team.Id)
@@ -415,8 +415,10 @@ func TestInviteMembers(t *testing.T) {
 	restrictTeamInvite := *utils.Cfg.TeamSettings.RestrictTeamInvite
 	defer func() {
 		*utils.Cfg.TeamSettings.RestrictTeamInvite = restrictTeamInvite
+		utils.SetDefaultRolesBasedOnConfig()
 	}()
 	*utils.Cfg.TeamSettings.RestrictTeamInvite = model.PERMISSIONS_TEAM_ADMIN
+	utils.SetDefaultRolesBasedOnConfig()
 
 	th.LoginBasic2()
 	LinkUserToTeam(th.BasicUser2, team)
@@ -445,6 +447,7 @@ func TestInviteMembers(t *testing.T) {
 	}
 
 	*utils.Cfg.TeamSettings.RestrictTeamInvite = model.PERMISSIONS_SYSTEM_ADMIN
+	utils.SetDefaultRolesBasedOnConfig()
 
 	if _, err := Client.InviteMembers(invites); err == nil {
 		t.Fatal("should have errored not system admin and licensed")

store/sql_user_store_test.go
@@ -342,7 +342,7 @@ func TestUserStoreGetSystemAdminProfiles(t *testing.T) {
 
 	u1 := &model.User{}
 	u1.Email = model.NewId()
-	u1.Roles = model.ROLE_SYSTEM_ADMIN
+	u1.Roles = model.R_SYSTEM_USER.Id + " " + model.R_SYSTEM_ADMIN.Id
 	Must(store.User().Save(u1))
 	Must(store.Team().SaveMember(&model.TeamMember{TeamId: teamId, UserId: u1.Id}))
 

wget/mattermost-platform

api/authorization_test.go
@@ -1,0 +1,36 @@
+// Copyright (c) 2016 Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+package api
+
+import (
+	"testing"
+
+	"github.com/mattermost/platform/model"
+)
+
+func TestCheckIfRolesGrantPermission(t *testing.T) {
+	Setup()
+
+	cases := []struct {
+		roles        []string
+		permissionId string
+		shouldGrant  bool
+	}{
+		{[]string{model.ROLE_SYSTEM_ADMIN.Id}, model.ROLE_SYSTEM_ADMIN.Permissions[0], true},
+		{[]string{model.ROLE_SYSTEM_ADMIN.Id}, "non-existant-permission", false},
+		{[]string{model.ROLE_CHANNEL_USER.Id}, model.ROLE_CHANNEL_USER.Permissions[0], true},
+		{[]string{model.ROLE_CHANNEL_USER.Id}, model.PERMISSION_MANAGE_SYSTEM.Id, false},
+		{[]string{model.ROLE_SYSTEM_ADMIN.Id, model.ROLE_CHANNEL_USER.Id}, model.PERMISSION_MANAGE_SYSTEM.Id, true},
+		{[]string{model.ROLE_CHANNEL_USER.Id, model.ROLE_SYSTEM_ADMIN.Id}, model.PERMISSION_MANAGE_SYSTEM.Id, true},
+		{[]string{model.ROLE_TEAM_USER.Id, model.ROLE_TEAM_ADMIN.Id}, model.PERMISSION_MANAGE_SLASH_COMMANDS.Id, true},
+		{[]string{model.ROLE_TEAM_ADMIN.Id, model.ROLE_TEAM_USER.Id}, model.PERMISSION_MANAGE_SLASH_COMMANDS.Id, true},
+	}
+
+	for testnum, testcase := range cases {
+		if CheckIfRolesGrantPermission(testcase.roles, testcase.permissionId) != testcase.shouldGrant {
+			t.Fatal("Failed test case ", testnum)
+		}
+	}
+
+}

api/context.go
@@ -16,7 +16,6 @@ import (
 
 	"github.com/mattermost/platform/einterfaces"
 	"github.com/mattermost/platform/model"
-	"github.com/mattermost/platform/store"
 	"github.com/mattermost/platform/utils"
 )
 
@@ -75,6 +74,10 @@ func AppHandlerTrustRequester(h func(*Context, http.ResponseWriter, *http.Reques
 
 func ApiAdminSystemRequired(h func(*Context, http.ResponseWriter, *http.Request)) http.Handler {
 	return &handler{h, true, true, true, false, false, false}
+}
+
+func ApiAdminSystemRequiredTrustRequester(h func(*Context, http.ResponseWriter, *http.Request)) http.Handler {
+	return &handler{h, true, true, true, false, false, true}
 }
 
 func ApiAppHandlerTrustRequester(h func(*Context, http.ResponseWriter, *http.Request)) http.Handler {
@@ -202,10 +205,6 @@ func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		c.SystemAdminRequired()
 	}
 
-	if c.Err == nil && len(c.TeamId) > 0 && !h.isTeamIndependent {
-		c.HasPermissionsToTeam(c.TeamId, "TeamRoute")
-	}
-
 	if c.Err == nil && h.isUserActivity && token != "" && len(c.Session.UserId) > 0 {
 		SetStatusOnline(c.Session.UserId, c.Session.Id)
 	}
@@ -320,88 +319,11 @@ func (c *Context) SystemAdminRequired() {
 		c.Err = model.NewLocAppError("", "api.context.session_expired.app_error", nil, "SystemAdminRequired")
 		c.Err.StatusCode = http.StatusUnauthorized
 		return
-	} else if !c.IsSystemAdmin() {
+	} else if !HasPermissionToContext(c, model.PERMISSION_MANAGE_SYSTEM) {
 		c.Err = model.NewLocAppError("", "api.context.permissions.app_error", nil, "AdminRequired")
 		c.Err.StatusCode = http.StatusForbidden
 		return
 	}
-}
-
-func (c *Context) HasPermissionsToUser(userId string, where string) bool {
-
-	// You are the user
-	if c.Session.UserId == userId {
-		return true
-	}
-
-	// You're a mattermost system admin and you're on the VPN
-	if c.IsSystemAdmin() {
-		return true
-	}
-
-	c.Err = model.NewLocAppError(where, "api.context.permissions.app_error", nil, "userId="+userId)
-	c.Err.StatusCode = http.StatusForbidden
-	return false
-}
-
-func (c *Context) HasPermissionsToTeam(teamId string, where string) bool {
-	if c.IsSystemAdmin() {
-		return true
-	}
-
-	for _, teamMember := range c.Session.TeamMembers {
-		if teamId == teamMember.TeamId {
-			return true
-		}
-	}
-
-	c.Err = model.NewLocAppError(where, "api.context.permissions.app_error", nil, "userId="+c.Session.UserId+", teamId="+teamId)
-	c.Err.StatusCode = http.StatusForbidden
-	return false
-}
-
-func (c *Context) HasPermissionsToChannel(sc store.StoreChannel, where string) bool {
-	if cresult := <-sc; cresult.Err != nil {
-		c.Err = cresult.Err
-		return false
-	} else if cresult.Data.(int64) != 1 {
-		c.Err = model.NewLocAppError(where, "api.context.permissions.app_error", nil, "userId="+c.Session.UserId)
-		c.Err.StatusCode = http.StatusForbidden
-		return false
-	}
-
-	return true
-}
-
-func (c *Context) HasSystemAdminPermissions(where string) bool {
-	if c.IsSystemAdmin() {
-		return true
-	}
-
-	c.Err = model.NewLocAppError(where, "api.context.system_permissions.app_error", nil, "userId="+c.Session.UserId)
-	c.Err.StatusCode = http.StatusForbidden
-	return false
-}
-
-func (c *Context) IsSystemAdmin() bool {
-	if model.IsInRole(c.Session.Roles, model.ROLE_SYSTEM_ADMIN) {
-		return true
-	}
-	return false
-}
-
-func (c *Context) IsTeamAdmin() bool {
-
-	if c.IsSystemAdmin() {
-		return true
-	}
-
-	teamMember := c.Session.GetTeamByTeamId(c.TeamId)
-	if teamMember == nil {
-		return false
-	}
-
-	return teamMember.IsTeamAdmin()
 }
 
 func (c *Context) RemoveSessionCookie(w http.ResponseWriter, r *http.Request) {
@@ -461,6 +383,10 @@ func (c *Context) GetTeamURL() string {
 
 func (c *Context) GetSiteURL() string {
 	return c.siteURL
+}
+
+func (c *Context) GetCurrentTeamMember() *model.TeamMember {
+	return c.Session.GetTeamByTeamId(c.TeamId)
 }
 
 func IsApiCall(r *http.Request) bool {

api/emoji.go
@@ -182,7 +182,7 @@ func deleteEmoji(c *Context, w http.ResponseWriter, r *http.Request) {
 		c.Err = result.Err
 		return
 	} else {
-		if c.Session.UserId != result.Data.(*model.Emoji).CreatorId && !c.IsSystemAdmin() {
+		if c.Session.UserId != result.Data.(*model.Emoji).CreatorId && !HasPermissionToContext(c, model.PERMISSION_MANAGE_SYSTEM) {
 			c.Err = model.NewLocAppError("deleteEmoji", "api.emoji.delete.permissions.app_error", nil, "user_id="+c.Session.UserId)
 			c.Err.StatusCode = http.StatusUnauthorized
 			return

api/post.go
@@ -58,10 +58,24 @@ func createPost(c *Context, w http.ResponseWriter, r *http.Request) {
 	}
 	post.UserId = c.Session.UserId
 
-	// Create and save post object to channel
-	cchan := Srv.Store.Channel().CheckPermissionsTo(c.TeamId, post.ChannelId, c.Session.UserId)
+	cchan := Srv.Store.Channel().Get(post.ChannelId)
 
-	if !c.HasPermissionsToChannel(cchan, "createPost") {
+	if !HasPermissionToChannelContext(c, post.ChannelId, model.PERMISSION_CREATE_POST) {
+		return
+	}
+
+	// Check that channel has not been deleted
+	var channel *model.Channel
+	if result := <-cchan; result.Err != nil {
+		c.SetInvalidParam("createPost", "post.channelId")
+		return
+	} else {
+		channel = result.Data.(*model.Channel)
+	}
+
+	if channel.DeleteAt != 0 {
+		c.Err = model.NewLocAppError("createPost", "api.post.create_post.can_not_post_to_deleted.error", nil, "")
+		c.Err.StatusCode = http.StatusBadRequest
 		return
 	}
 
@@ -1032,10 +1046,9 @@ func updatePost(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	cchan := Srv.Store.Channel().CheckPermissionsTo(c.TeamId, post.ChannelId, c.Session.UserId)
 	pchan := Srv.Store.Post().Get(post.Id)
 
-	if !c.HasPermissionsToChannel(cchan, "updatePost") {
+	if !HasPermissionToChannelContext(c, post.ChannelId, model.PERMISSION_EDIT_POST) {
 		return
 	}
 
@@ -1137,10 +1150,9 @@ func getPosts(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	cchan := Srv.Store.Channel().CheckPermissionsTo(c.TeamId, id, c.Session.UserId)
 	etagChan := Srv.Store.Post().GetEtag(id)
 
-	if !c.HasPermissionsToChannel(cchan, "getPosts") {
+	if !HasPermissionToChannelContext(c, id, model.PERMISSION_CREATE_POST) {
 		return
 	}
 
@@ -1179,10 +1191,9 @@ func getPostsSince(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	cchan := Srv.Store.Channel().CheckPermissionsTo(c.TeamId, id, c.Session.UserId)
 	pchan := Srv.Store.Post().GetPostsSince(id, time)
 
-	if !c.HasPermissionsToChannel(cchan, "getPostsSince") {
+	if !HasPermissionToChannelContext(c, id, model.PERMISSION_READ_CHANNEL) {
 		return
 	}
 
@@ -1212,10 +1223,9 @@ func getPost(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	cchan := Srv.Store.Channel().CheckPermissionsTo(c.TeamId, channelId, c.Session.UserId)
 	pchan := Srv.Store.Post().Get(postId)
 
-	if !c.HasPermissionsToChannel(cchan, "getPost") {
+	if !HasPermissionToChannelContext(c, channelId, model.PERMISSION_READ_CHANNEL) {
 		return
 	}
 
@@ -1259,8 +1269,7 @@ func getPostById(c *Context, w http.ResponseWriter, r *http.Request) {
 		}
 		post := list.Posts[list.Order[0]]
 
-		cchan := Srv.Store.Channel().CheckPermissionsTo(c.TeamId, post.ChannelId, c.Session.UserId)
-		if !c.HasPermissionsToChannel(cchan, "getPostById") {
+		if !HasPermissionToChannelContext(c, post.ChannelId, model.PERMISSION_READ_CHANNEL) {
 			return
 		}
 
@@ -1294,12 +1303,7 @@ func getPermalinkTmp(c *Context, w http.ResponseWriter, r *http.Request) {
 		}
 		post := list.Posts[list.Order[0]]
 
-		if !c.HasPermissionsToTeam(c.TeamId, "permalink") {
-			return
-		}
-
-		cchan := Srv.Store.Channel().CheckPermissionsTo(c.TeamId, post.ChannelId, c.Session.UserId)
-		if !c.HasPermissionsToChannel(cchan, "getPermalinkTmp") {
+		if !HasPermissionToChannelContext(c, post.ChannelId, model.PERMISSION_READ_CHANNEL) {
 			// If we don't have permissions attempt to join the channel to fix the problem
 			if err, _ := JoinChannelById(c, c.Session.UserId, post.ChannelId); err != nil {
 				// On error just return with permissions error
@@ -1335,7 +1339,10 @@ func deletePost(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	cchan := Srv.Store.Channel().CheckPermissionsTo(c.TeamId, channelId, c.Session.UserId)
+	if !HasPermissionToChannelContext(c, channelId, model.PERMISSION_EDIT_POST) {
+		return
+	}
+
 	pchan := Srv.Store.Post().Get(postId)
 
 	if result := <-pchan; result.Err != nil {
@@ -1344,10 +1351,6 @@ func deletePost(c *Context, w http.ResponseWriter, r *http.Request) {
 	} else {
 
 		post := result.Data.(*model.PostList).Posts[postId]
-
-		if !c.HasPermissionsToChannel(cchan, "deletePost") && !c.IsTeamAdmin() {
-			return
-		}
 
 		if post == nil {
 			c.SetInvalidParam("deletePost", "postId")
@@ -1360,7 +1363,7 @@ func deletePost(c *Context, w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		if post.UserId != c.Session.UserId && !c.IsTeamAdmin() {
+		if post.UserId != c.Session.UserId && !HasPermissionToChannelContext(c, post.ChannelId, model.PERMISSION_EDIT_OTHERS_POSTS) {
 			c.Err = model.NewLocAppError("deletePost", "api.post.delete_post.permissions.app_error", nil, "")
 			c.Err.StatusCode = http.StatusForbidden
 			return
@@ -1440,11 +1443,10 @@ func getPostsBeforeOrAfter(c *Context, w http.ResponseWriter, r *http.Request, b
 		return
 	}
 
-	cchan := Srv.Store.Channel().CheckPermissionsTo(c.TeamId, id, c.Session.UserId)
 	// We can do better than this etag in this situation
 	etagChan := Srv.Store.Post().GetEtag(id)
 
-	if !c.HasPermissionsToChannel(cchan, "getPostsBeforeOrAfter") {
+	if !HasPermissionToChannelContext(c, id, model.PERMISSION_READ_CHANNEL) {
 		return
 	}
 

api/team_test.go
@@ -175,14 +175,14 @@ func TestRemoveUserFromTeam(t *testing.T) {
 		t.Fatal("should fail not enough permissions")
 	} else {
 		if err.Id != "api.context.permissions.app_error" {
-			t.Fatal("wrong error")
+			t.Fatal("wrong error. Got: " + err.Id)
 		}
 	}
 
 	if _, err := th.BasicClient.RemoveUserFromTeam("", th.SystemAdminUser.Id); err == nil {
 		t.Fatal("should fail not enough permissions")
 	} else {
-		if err.Id != "api.team.update_team.permissions.app_error" {
+		if err.Id != "api.context.permissions.app_error" {
 			t.Fatal("wrong error")
 		}
 	}
@@ -318,7 +318,7 @@ func TestGetAllTeamListings(t *testing.T) {
 	c := &Context{}
 	c.RequestId = model.NewId()
 	c.IpAddress = "cmd_line"
-	UpdateUserRoles(c, user, model.ROLE_SYSTEM_ADMIN)
+	UpdateUserRoles(c, user, model.ROLE_SYSTEM_ADMIN.Id)
 
 	Client.Login(user.Email, "passwd1")
 	Client.SetTeamId(team.Id)
@@ -415,8 +415,10 @@ func TestInviteMembers(t *testing.T) {
 	restrictTeamInvite := *utils.Cfg.TeamSettings.RestrictTeamInvite
 	defer func() {
 		*utils.Cfg.TeamSettings.RestrictTeamInvite = restrictTeamInvite
+		utils.SetDefaultRolesBasedOnConfig()
 	}()
 	*utils.Cfg.TeamSettings.RestrictTeamInvite = model.PERMISSIONS_TEAM_ADMIN
+	utils.SetDefaultRolesBasedOnConfig()
 
 	th.LoginBasic2()
 	LinkUserToTeam(th.BasicUser2, team)
@@ -445,6 +447,7 @@ func TestInviteMembers(t *testing.T) {
 	}
 
 	*utils.Cfg.TeamSettings.RestrictTeamInvite = model.PERMISSIONS_SYSTEM_ADMIN
+	utils.SetDefaultRolesBasedOnConfig()
 
 	if _, err := Client.InviteMembers(invites); err == nil {
 		t.Fatal("should have errored not system admin and licensed")

store/sql_user_store_test.go
@@ -342,7 +342,7 @@ func TestUserStoreGetSystemAdminProfiles(t *testing.T) {
 
 	u1 := &model.User{}
 	u1.Email = model.NewId()
-	u1.Roles = model.ROLE_SYSTEM_ADMIN
+	u1.Roles = model.ROLE_SYSTEM_USER.Id + " " + model.ROLE_SYSTEM_ADMIN.Id
 	Must(store.User().Save(u1))
 	Must(store.Team().SaveMember(&model.TeamMember{TeamId: teamId, UserId: u1.Id}))
 